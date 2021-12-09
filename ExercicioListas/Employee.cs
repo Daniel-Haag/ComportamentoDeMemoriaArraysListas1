cf670cfbd647b4720c12e92f4ec8bf4aabf64954
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:34:23 -0300

    primeiro commit

---
+++ ExercicioListas/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ExercicioListas
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(Employee employee, double percentage)
+        {
+            this.Salary += employee.Salary * percentage / 100.0;
+        }
+    }
+}
---
+++ ExercicioListas/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioListas
+{
+    class Program
+    {
+        static void Main()
+        {
+            int id;
+            string name;
+            double salary;
+            bool repeat = false;
+            double percentage;
+
+            Console.WriteLine("How many employees will be registered?");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+            List<Employee> employeesList = new List<Employee>();
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i + 1}");
+                do
+                {
+                    Console.Write("ID: ");
+                    id = int.Parse(Console.ReadLine());
+
+                    var test = employeesList.Find(x => x.ID == id);
+
+                    if (test != null)
+                    {
+                        Console.WriteLine("ID already exist, create another one.");
+                        repeat = true;
+                    }
+                    else
+                    {
+                        repeat = false;
+                    }
+
+                } while (repeat);
+
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Salary: ");
+                salary = double.Parse(Console.ReadLine());
+
+                Employee employee = new Employee(id, name, salary);
+
+                employeesList.Add(employee);
+            }
+
+            Console.Write("Enter the employee ID that will have salary increase: ");
+            id = int.Parse(Console.ReadLine());
+
+            Employee employee1 = employeesList.Find(x => x.ID == id);
+
+            Console.WriteLine("Enter the percentage: ");
+            percentage = double.Parse(Console.ReadLine());
+
+            employee1.IncreaseSalary(employee1, percentage);
+
+            Console.WriteLine("Updated list of employees:");
+
+            foreach(var item in employeesList)
+            {
+                Console.WriteLine($"{item.ID},{item.Name},{item.Salary}");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
